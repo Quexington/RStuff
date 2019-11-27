fa2fed09a678d0e6b555a0579f72291032ab2d17
Quexington <quexington@gmail.com>
2019-11-27 05:46:40 -0700

    Did some more stuff

--- workspace.r
+++ workspace.r
@@ -1,14 +1,57 @@
-csv <- read.csv('C:/users/quexi/Documents/RStuff/csvs/advanced_whole_nba_season_stats.csv', header=TRUE)
-current <- read.csv('C:/users/quexi/Documents/RStuff/csvs/current_nba_season_stats.csv', header=TRUE)
-sorted = csv[order(csv$Season, decreasing=TRUE), ]
-neo_csv = sorted[31:180, ]
-reg = lm(neo_csv[,8]~.,data=neo_csv[,9:40])
-
-for(i in 1:30){
-  ontrack = current[i,9:26]*82/current[i,5]
-  adjusted = c(current[i,1:8],ontrack,current[i,27:40])
-  current[i,] = adjusted
+train <- read.csv('./kaggle_comps/csvs/train.csv')
+test <- read.csv('./kaggle_comps/csvs/test.csv')
+vector <- c()
+for(i in 1:891) {
+  vector <- c(vector, substr(train[i,11],1,1))
 }
-predictions = predict(reg, newdata=current[,9:40])
-answer = data.frame(current[,3],predictions*82)
-print(answer[order(answer$predictions, decreasing=TRUE),])
+train[,11] <- vector
+vector <- c()
+for(i in 1:891) {
+  vector <- c(vector, as.character(train[i,3]))
+}
+train[,3] <- vector
+vector <- c()
+for(i in 1:891) {
+  vector <- c(vector, as.character(train[i,2]))
+}
+train[,2] <- vector
+vector <- c()
+for(i in 1:891) {
+  vector <- c(vector, as.integer(train[i,9]))
+}
+train[,9] <- vector
+
+vector <- c()
+for(i in 1:418) {
+  vector <- c(vector, substr(test[i,10],1,1))
+}
+test[,10] <- vector
+vector <- c()
+for(i in 1:418) {
+  vector <- c(vector, as.character(test[i,2]))
+}
+test[,2] <- vector
+vector <- c()
+for(i in 1:418) {
+  vector <- c(vector, as.integer(test[i,8]))
+}
+test[,8] <- vector
+
+results <- train[,2]
+factors <- train[,c(3,5:12)]
+reg <- lm(results~.,data=factors)
+predictions <- predict(reg, newdata=test[,c(2,4:11)])
+answer = data.frame(test[,1],predictions)
+comp <- answer[order(answer[,2], decreasing=TRUE),][1:160,][,1]
+
+vector <- c()
+for (i in 892:1309) {
+  if (is.na(match(i,comp))) {
+    vector <- c(vector, 0)
+  } else {
+    vector <- c(vector, 1)
+  }
+}
+ans <- data.frame(c(892:1309),vector)
+write.csv(ans, file='./kaggle_comps/csvs/answer.csv', row.names=FALSE)
+#print(answer)
